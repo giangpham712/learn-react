04478bdae3bcfc1f38806e82de5eee51c12342fc
Giang Pham <giang.phamhoang712@gmail.com>
2023-04-29 13:29:44 +1000

    chore: add new Vite React and Remix apps

---
+++ packages/eslint-config-custom-next/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ["custom", "next"],
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+  },
+};
