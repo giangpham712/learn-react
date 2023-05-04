5f6d131d04095d2d6940d075ecf9bab26ab4a10c
Giang Pham <giang.phamhoang712@gmail.com>
2023-05-04 21:50:45 +1000

    chore: implement React Router example

---
+++ apps/vite-react/src/types.ts
@@ -0,0 +1,10 @@
+export interface Contact {
+  id?: string;
+  first?: string;
+  last?: string;
+  avatar?: string;
+  twitter?: string;
+  notes?: string;
+  createdAt?: number;
+  favorite?: boolean;
+}
